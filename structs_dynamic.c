70b9c9d70abe1eb0577641c23046bb2998ee0b47
Dhatta Sai Chanuka Sista <113626707+chanukya571@users.noreply.github.com>
2022-12-19 08:46:58 +0100

    formatted code

--- structs_dynamic.c
+++ structs_dynamic.c
@@ -2,57 +2,55 @@
 #include <string.h>
 #include <stdlib.h>
 
-int MAX=100;
+int MAX = 100;
 
 struct student
 {
     char *name;
     int ID;
-    
-}s1;
 
-int main() {
+} s1;
+
+int main()
+{
 
     struct student *ptr;
-    
+
     int noOfStudents;
-    
+
     printf("Entre number of records you want to store:");
-    
-    scanf("%d",&noOfStudents);
+
+    scanf("%d", &noOfStudents);
 
     char sname[MAX];
-    
-    ptr = (struct student *) malloc(noOfStudents * sizeof(struct student));
 
-    for (int i=0; i<noOfStudents;i++)
+    ptr = (struct student *)malloc(noOfStudents * sizeof(struct student));
+
+    for (int i = 0; i < noOfStudents; i++)
     {
-        
+
         printf("\nEnter name of student:");
 
-        scanf("%s",sname);
+        scanf("%s", sname);
 
         int length = strlen(sname);
 
-        (ptr+i)->name = (char*)malloc(strlen(sname)+1);
+        (ptr + i)->name = (char *)malloc(strlen(sname) + 1);
 
-        strcpy((ptr+i)->name,sname);
+        strcpy((ptr + i)->name, sname);
 
         printf("Enter ID:");
 
-        scanf("%d",&(ptr + i) -> ID);
-
+        scanf("%d", &(ptr + i)->ID);
     }
-    
+
     printf("\nStudent Board\n");
-    for (int i=0; i<noOfStudents;i++)
+    for (int i = 0; i < noOfStudents; i++)
     {
-        printf("\n%d\t%s",(ptr+i)->ID, (ptr+i)->name);
+        printf("\n%d\t%s", (ptr + i)->ID, (ptr + i)->name);
 
         printf("\n");
     }
 
     free(ptr);
-
-
 }
